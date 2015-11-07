97cb798340494f04193df49917240d1fc4ec2729
LKHohn <lkhohn@gmail.com>
2015-11-07 11:58:27 -0700

    Lindsay's commit

--- directory.js
+++ directory.js
@@ -1,5 +1,17 @@
 var directory = {
-  jim: "Bulb",
-  sara: "Leading Learners",
-  ray: "Chem with Ray"
+  jim: 'Bulb',
+  sara: 'Leading Learners',
+  ray: 'Chem with Ray'
+};
+
+var input = window.prompt("Who are you looking for?")
+
+
+function searchGalvanize (input){
+  for(var key in directory){
+    if(key===input){
+      return directory[input];
+    }
 }
+}
+console.log(searchGalvanize(input));
